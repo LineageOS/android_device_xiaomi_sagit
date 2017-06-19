bdc4b4d4d945c1c1a1803a0d18977e6cbc69c9d0
xiaobai <fengz181x@126.com>
2017-06-20 05:55:59 +0800

    sagit: fix dt2w

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -116,6 +116,7 @@ BOARD_HARDWARE_CLASS += \
     hardware/cyanogen/cmhw \
     $(DEVICE_PATH)/cmhw
 BOARD_USES_CYANOGEN_HARDWARE := true
+TARGET_TAP_TO_WAKE_NODE :=  "/sys/devices/soc/c179000.i2c/i2c-5/5-0020/input/input1/wake_gesture"
 
 # CNE and DPM
 BOARD_USES_QCNE := true
