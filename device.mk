e8dec67ae89eeee8a6085f4f84c65d20efa0c907
Luca Stefani <luca020400@lineageos.org>
2017-08-23 16:40:39 +0200

    sagit: Add seccomp policy
    Change-Id: Ief4d3442c395e7bb451bf106f48c01e921a69631

--- device.mk
+++ device.mk
@@ -268,6 +268,11 @@ PRODUCT_PACKAGES += \
     libxml2 \
     libprotobuf-cpp-full
 
+# Seccomp policy
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:system/vendor/etc/seccomp_policy/mediacodec.policy \
+    $(LOCAL_PATH)/seccomp_policy/mediaextractor.policy:system/vendor/etc/seccomp_policy/mediaextractor.policy
+
 # Sensors
 PRODUCT_PACKAGES += \
     sensors.msm8998
