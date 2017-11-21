ae732e6a7c20bb653e6c3c657a2f9cc2156bdd1f
Rashed Abdel-Tawab <rashedabdeltawab@gmail.com>
2017-11-21 11:19:11 +0000

    board: fix 2nd cpu variant
    cortex-a53 is supported, use it.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -30,7 +30,7 @@ TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv7-a-neon
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := cortex-a9
+TARGET_2ND_CPU_VARIANT := cortex-a53
 
 ENABLE_CPUSETS := true
 
