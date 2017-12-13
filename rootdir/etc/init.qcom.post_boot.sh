e36c283ada65d296ea478005c92b2da08c43cfab
Demon000 <demonsingur@gmail.com>
2017-12-13 20:25:26 +0200

    post_boot: allow turning off all big cores when not active
    Change-Id: Ic5ddf9978cb435a48ecefd069d403dad3ab78884

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -27,7 +27,8 @@
 #
 
 # Configure core ctl
-echo 2 > /sys/devices/system/cpu/cpu4/core_ctl/min_cpus
+echo 0 > /sys/devices/system/cpu/cpu4/core_ctl/min_cpus
+echo 4 > /sys/devices/system/cpu/cpu4/core_ctl/max_cpus
 echo 60 > /sys/devices/system/cpu/cpu4/core_ctl/busy_up_thres
 echo 30 > /sys/devices/system/cpu/cpu4/core_ctl/busy_down_thres
 echo 100 > /sys/devices/system/cpu/cpu4/core_ctl/offline_delay_ms
