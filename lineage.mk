68babd657f2c8e5340901b82a13ca96339594938
Michael Bestas <mkbestas@lineageos.org>
2018-01-27 06:39:34 +0200

    sagit: Add latest global stable build fingerprint
    Change-Id: I508803723c9dce25ebdaaaa5d22abf17fa4503fd

--- lineage.mk
+++ lineage.mk
@@ -10,3 +10,7 @@ PRODUCT_MODEL := MI 6
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT=Xiaomi/sagit/sagit:7.1.1/NMF26X/V9.2.2.0.NCAMIEK:user/release-keys \
+    PRIVATE_BUILD_DESC="sagit-user 7.1.1 NMF26X V9.2.2.0.NCAMIEK release-keys"
