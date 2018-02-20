0b755b441593c82bfd66a10689017bcbdb4a5979
Michael Bestas <mkbestas@lineageos.org>
2018-02-21 00:28:53 +0200

    sagit: Make copyright header style consistent
    Change-Id: I865e8fb0fbe68942c151f874133311a942238a44

--- Android.mk
+++ Android.mk
@@ -12,12 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-# WARNING: Everything listed here will be built on ALL platforms,
-# including x86, the emulator, and the SDK.  Modules must be uniquely
-# named (liblights.tuna), and must build everywhere, or limit themselves
-# to only building on ARM if they include assembly. Individual makefiles
-# are responsible for having their own logic, for fine-grained control.
+#
 
 LOCAL_PATH := $(call my-dir)
 
