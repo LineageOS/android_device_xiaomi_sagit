e708149cb4e3719553836a4424c4b6eb7ff8e9f6
Demon Singur <demonsingur@gmail.com>
2018-03-06 18:56:42 +0000

    sagit: add ir hidl service and manifest entry
    Change-Id: I77931c8eb01e9d0b28a7a59b6aa21e45d0fafbf0

--- device.mk
+++ device.mk
@@ -33,6 +33,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/keylayout/synaptics_dsx.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/synaptics_dsx.kl
 
+# IR
+PRODUCT_PACKAGES += \
+    android.hardware.ir@1.0-impl \
+    android.hardware.ir@1.0-service
+
 # NFC
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
