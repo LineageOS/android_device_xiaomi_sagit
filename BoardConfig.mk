d303c2c3d7e2edad50d4b9583722e43a4222890c
Demon Singur <demonsingur@gmail.com>
2018-03-06 18:56:42 +0000

    sagit: add ir hidl service and manifest entry
    Change-Id: I77931c8eb01e9d0b28a7a59b6aa21e45d0fafbf0

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,6 +19,9 @@ include device/xiaomi/msm8998-common/BoardConfigCommon.mk
 
 DEVICE_PATH := device/xiaomi/sagit
 
+# HIDL
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
+
 # Kernel
 TARGET_KERNEL_CONFIG := sagit_defconfig
 
