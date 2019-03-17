cd495b48cb15a1856bc35af5347facf949b82478
Michael Bestas <mkbestas@lineageos.org>
2019-03-17 17:57:07 +0200

    sagit: Update build fingerprint from V10.2.1.0
    Change-Id: I2042d08478b7ffb6aa46c6ee83e8827fbb067dcc

--- lineage_sagit.mk
+++ lineage_sagit.mk
@@ -33,6 +33,6 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="sagit-user 8.0.0 OPR1.170623.027 V9.2.3.0.OCAMIEK release-keys"
+    PRIVATE_BUILD_DESC="sagit-user 8.0.0 OPR1.170623.027 V10.2.1.0.OCAMIXM release-keys"
 
-BUILD_FINGERPRINT := Xiaomi/sagit/sagit:8.0.0/OPR1.170623.027/V9.2.3.0.OCAMIEK:user/release-keys
+BUILD_FINGERPRINT := Xiaomi/sagit/sagit:8.0.0/OPR1.170623.027/V10.2.1.0.OCAMIXM:user/release-keys
