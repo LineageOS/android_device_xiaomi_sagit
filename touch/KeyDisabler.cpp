00d3c8b60dc41e2ddcb14acdc1ba8c3e9e6df640
Michael Bestas <mkbestas@lineageos.org>
2019-03-27 22:20:21 +0200

    sagit: touch: Adapt for new kernel
    Change-Id: I3e5f0677ef2b79eaa452fe38dcc9580968440ab8

--- touch/KeyDisabler.cpp
+++ touch/KeyDisabler.cpp
@@ -27,59 +27,30 @@ namespace V1_0 {
 namespace implementation {
 
 constexpr const char kControlPath[] = "/proc/touchpanel/capacitive_keys_enable";
-constexpr const char kFpcPath[] = "/sys/devices/soc/soc:fingerprint_fpc/enable_key_events";
-constexpr const char kGoodixPath[] = "/sys/devices/soc/soc:fingerprint_goodix/enable_key_events";
 
 KeyDisabler::KeyDisabler() {
-    mHasKeyDisabler = !access(kControlPath, F_OK)
-            && !access(kFpcPath, F_OK) && !access(kGoodixPath, F_OK);
+    mHasKeyDisabler = !access(kControlPath, F_OK);
 }
 
 // Methods from ::vendor::lineage::touch::V1_0::IKeyDisabler follow.
 Return<bool> KeyDisabler::isEnabled() {
-    std::string controlBuf;
-    std::string fpcBuf;
-    std::string goodixBuf;
+    std::string buf;
 
     if (!mHasKeyDisabler) return false;
 
-    if (!android::base::ReadFileToString(kControlPath, &controlBuf)) {
+    if (!android::base::ReadFileToString(kControlPath, &buf)) {
         LOG(ERROR) << "Failed to read " << kControlPath;
         return false;
     }
 
-    if (!android::base::ReadFileToString(kFpcPath, &fpcBuf)) {
-        LOG(ERROR) << "Failed to read " << kFpcPath;
-        return false;
-    }
-
-    if (!android::base::ReadFileToString(kGoodixPath, &goodixBuf)) {
-        LOG(ERROR) << "Failed to read " << kGoodixPath;
-        return false;
-    }
-
-    return std::stoi(android::base::Trim(controlBuf)) == 0
-            || std::stoi(android::base::Trim(fpcBuf)) == 0
-            || std::stoi(android::base::Trim(goodixBuf)) == 0;
+    return std::stoi(android::base::Trim(buf)) == 0;
 }
 
 Return<bool> KeyDisabler::setEnabled(bool enabled) {
-    std::string buf = enabled ? "0" : "1";
-
     if (!mHasKeyDisabler) return false;
 
-    if (!android::base::WriteStringToFile(buf, kControlPath)) {
+    if (!android::base::WriteStringToFile((enabled ? "0" : "1"), kControlPath)) {
         LOG(ERROR) << "Failed to write " << kControlPath;
-        return false;
-    }
-
-    if (!android::base::WriteStringToFile(buf, kFpcPath)) {
-        LOG(ERROR) << "Failed to write " << kFpcPath;
-        return false;
-    }
-
-    if (!android::base::WriteStringToFile(buf, kGoodixPath)) {
-        LOG(ERROR) << "Failed to write " << kGoodixPath;
         return false;
     }
 
