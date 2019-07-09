4d0bb61dd8635b7bef68da20a390ed721bca4170
Bruno Martins <bgcngm@gmail.com>
2019-07-09 22:31:00 +0100

    sagit: configpanel: Migrate to androidx
    Change-Id: Ifaa67283a3437549a2304088a0ce377b399da19f
    Signed-off-by: Arian <arian.kulmer@web.de>

--- configpanel/Android.mk
+++ configpanel/Android.mk
@@ -10,12 +10,8 @@ LOCAL_PRIVATE_PLATFORM_APIS := true
 LOCAL_USE_AAPT2 := true
 
 LOCAL_STATIC_ANDROID_LIBRARIES := \
-    android-support-v4 \
-    android-support-v7-appcompat \
-    android-support-v7-preference \
-    android-support-v7-recyclerview \
-    android-support-v13 \
-    android-support-v14-preference \
+    androidx.core_core \
+    androidx.preference_preference
 
 LOCAL_STATIC_JAVA_LIBRARIES := \
     org.lineageos.platform.internal
