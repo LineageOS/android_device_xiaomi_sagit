4b8bf2287f5f5ddf8843b51c263a36527864530d
Michael Bestas <mkbestas@lineageos.org>
2020-11-01 17:38:06 +0200

    Revert "sagit: Exclude lineage-sdk overlays from RRO"
    This reverts commit 1e6ae803348c211d4e4989911b475fd645430a23.
    
    Change-Id: Ic3cf99d5b1223e48d259b29757d844078a7c3cfb

--- device.mk
+++ device.mk
@@ -22,9 +22,6 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage/lineage-sdk
-
 # Inherit from msm8998-common
 $(call inherit-product, device/xiaomi/msm8998-common/msm8998.mk)
 
